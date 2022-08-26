bf87bcba7f4784b9cba5b96e7347933246732d81
Marco Oliveira <marco.aurelio@nomadglobal.com>
2022-08-26 10:16:41 -0300

    insert request location and Geolocation

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Onebrain Company on 26/08/22.
+//
+
+import Foundation
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
-import {ImageBackground, SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  ImageBackground,
+  PermissionsAndroid,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
 import {images} from '../../assets/images';
 import {Header} from '../../components/Header/Header';
 
 export const HomeScreen = () => {
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+
+  const getPosition = useCallback(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, []);
+
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      Geolocation.requestAuthorization('whenInUse').then(() => {
+        getPosition();
+      });
+    } else {
+      const requestLocationPermission = async () => {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              title: 'Precisamos da sua permissão',
+              message:
+                'Para pegar sua lucalização, precisamos da sua permissão.',
+              buttonNeutral: 'Ask Me Later',
+              buttonNegative: 'Cancel',
+              buttonPositive: 'OK',
+            },
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            getPosition();
+          } else {
+            console.log('Negado');
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      };
+
+      requestLocationPermission();
+    }
+  }, [getPosition]);
+
   return (
     <ImageBackground source={images.backgroundImage} style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
